c4dad6e9a91b8d8caaa8efe2d0ee4932249aecc2
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-05-23 13:15:25 -0500

    Create basic resources

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,43 @@
+class CarsController < ApplicationController
+
+  # GET /cars
+  def index
+  end
+
+  # GET /cars/1.json
+  def show
+  end
+
+  # GET /cars/new
+  def new
+  end
+
+  # GET /cars/1/edit
+  def edit
+  end
+
+  # POST /cars or /cars.json
+  def create
+  end
+
+  # PATCH/PUT /cars/1 or /cars/1.json
+  def update
+  end
+
+  def update_car
+  end
+
+  # DELETE /cars/1 or /cars/1.json
+  def destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_car
+  end
+
+  # Only allow a list of trusted parameters through.
+  def car_params
+  end
+end
