dbfdf001dbd4f6d9521785a27311eadf1f05171e
Richard Tonata <rtonata@gmail.com>
2022-06-07 14:15:14 +0200

    Add fetching car details API documentation

--- spec/requests/api/cars_spec.rb
+++ spec/requests/api/cars_spec.rb
@@ -16,4 +16,36 @@ RSpec.describe 'cars' do
         end
     end
   end
+
+   path '/cars/{id}' do
+        get('Display a list of cars') do
+            tags 'Cars'
+            consumes 'application/json'
+            produces 'application/json'
+            parameter name: :id, in: :path, type: :string
+            response(200, 'List of cars found') do
+            schema type: :object,
+                properties: {
+                    name: { type: :string },
+                    make: { type: :string },
+                    image: { type: :string },
+                    model: { type: :string },
+                    description: { type: :string }
+                },
+                required: %w[name description make image model]
+
+                let(:id) do
+                { name: 'Vogue', 
+                    make: "Range Rover",
+                    description: 'The vehicle will be delivered and collected at no addtional cost. You can specify the exact location - hotel, address or the airport in the next step.', 
+                    image: "https://d2cnxbg536lf2p.cloudfront.net/test/file_5d14dc974de986_60230467.jpeg", 
+                    model: '2022' }
+                end
+                run_test!
+            end
+        end
+        response(404, 'Whoops!! Something went wrong') do
+            run_test!
+        end
+  end
 end
