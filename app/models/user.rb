ced8ea8c05cf2b80605bed3622d4e20cec272576
Oliver <olivercoimbra@gmail.com>
2022-06-07 16:42:07 -0400

    Fix errors of validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :favorites, dependent: :destroy
 
-  validates :username, password, presence: true
+  validates :username, length: { in: 5..15 }, presence: true
   validates :username, uniqueness:
-  { message: ':An account associated with %<username> already exists' }
+  { message: ':An account associated with this username already exists' }
   validates :password, length: { in: 5..15 }
-  validates :username, length: { in: 5..15 }
 end
