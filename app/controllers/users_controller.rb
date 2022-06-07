6d8a7549432c0037792cde75bb6c6d7e1ce775ac
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-06-07 15:47:08 -0500

    Minor fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.valid?
       token = JWT.encode({ user_id: @user.id }, SECRET, 'HS256')
-      render json: { user: @user, token: }
+      render json: { user: @user, token: token }
     else
       render json: { status: 500, errors: @user.errors.full_messages }
     end
