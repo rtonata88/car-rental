4cc78e9596ac8fc1534c84437754d203c9991041
Oliver <olivercoimbra@gmail.com>
2022-06-07 15:51:44 -0400

    Fix linter errors

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,10 +1,10 @@
 class Car < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
-  validates :car_id, numericality: { only_integer: true }
-  validates :name, presence: true
-  validates :make, presence: true
+  validates :name, length: { in: 2..15 }, presence: true
+  validates :make, length: { in: 2..15 }, presence: true
   validates :image, presence: true
-  validates :model, presence: true
-  validates :description, presence: true
+  validates :model, length: { in: 2..15 }, numericality: { only_integer: true }, presence: true
+  validates :description, length: { in: 10..180 },presence: true
+
 end
