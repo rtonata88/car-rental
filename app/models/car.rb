b1359c8e226ab01afe3c46cce8d79856dd16fe84
Oliver <olivercoimbra@gmail.com>
2022-06-07 13:40:48 -0400

    Add validation to cars

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,9 @@
 class Car < ApplicationRecord
   has_many :favorites, dependent: :destroy
-  
+
+  validates :name, presence: true
+  validates :make, presence: true
+  validates :image, presence: true
+  validates :model, presence: true
+  validates :description, presence: true
 end
