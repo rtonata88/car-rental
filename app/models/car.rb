4161773357c6252a8935852a494ef304a0816b8b
Oliver <olivercoimbra@gmail.com>
2022-06-07 14:41:29 -0400

    Add code reviewer suggestions

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,7 @@
 class Car < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
+  validates :car_id,  numericality: { only_integer: true }
   validates :name, presence: true
   validates :make, presence: true
   validates :image, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :favorites, dependent: :destroy
-
+  
   validates :username, password, presence: true
   validates :username, uniqueness:
   { message: ':An account associated with %{value} already exists' }
   validates :password, length: {in: 5..15}
+  validates :username, length: {in: 5..15}
 end
