43de79bf6a7ffe592d3bfb826f085a317a26fc68
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-06-08 15:09:21 -0500

    Typo fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
 
   validates :username, length: { in: 5..15 }, presence: true
   validates :username, uniqueness:
-  { message: ':An account associated with this username already exists' }
+  { message: 'An account associated with this username already exists' }
   validates :password, length: { in: 5..15 }
 end
